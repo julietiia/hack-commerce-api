e89e46f7cd6922f1727d00dbe38031352e9b20b5
julietiia <111154000+julietiia@users.noreply.github.com>
2023-11-28 18:15:53 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
-
 const userRoutes = require("./userRoutes");
-
-
-
-
 const publicRoutes = require("./publicRoutes");
 const privateRoutes = require("./privateRoutes");
 
 module.exports = (app) => {
-
   app.use("/usuarios", userRoutes);
-
-
   app.use("/", publicRoutes);
   app.use("/panel", privateRoutes);
 };
