613d474cfe552b8dfd5e4a2f57a37e2891ad7173
helmdyck <helmuth.dyck@gmail.com>
2023-12-05 22:34:49 -0300

    CategoryController added

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const productController = require ("../controllers/productController");
+const categoryController = require ("../controllers/categoryController")
 
 // Rutas relacionadas a la parte pública del sitio web:
 // ...
 router.get("/products", productController.index);
+router.get("/categories", categoryController.index);
 router.get("/category/:id", productController.getByCategory)
 module.exports = router;
